b09be9ff9464c837fa9e747e6107e788baa8c80f
miguelbalcazarr <miguel.balcazar@outlook.com>
2025-12-22 23:01:05 +0000

    sdp

---
+++ project_sdp/src/project_sdp_etl/transformations/bronze/clientes.py
@@ -0,0 +1,26 @@
+from pyspark.sql.functions import col
+from pyspark import pipelines as dp
+from project_sdp.src.project_sdp_etl.schemas.bronze import schema_clientes
+
+@dp.table(
+    name="clientes_raw",
+    comment="Tabla Bronze clientes_raw",
+    table_properties={
+        "quality": "bronze",
+        "pipelines.reset.allowed": "false",
+        "delta.appendOnly": "true",
+    },
+)
+
+def bronze_table():
+    df_reader = (
+        spark.readStream
+        .format("cloudFiles")
+        .option("cloudFiles.format", "csv")
+        .option("header", True)
+        .option("delimiter", ",")
+        .schema(schema_clientes())
+        .load("/Volumes/sdp/default/landing/clientes.csv")
+    )
+
+    return df_reader
